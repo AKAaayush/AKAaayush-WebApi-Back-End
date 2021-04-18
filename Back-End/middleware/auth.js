fa33cfe1ead0c0e215d27faa6f770b7382aef285
AKAaayush <shresthaaayush81@gmail.com>
2021-04-18 12:57:42 +0545

    android api

--- Back-End/middleware/auth.js
+++ Back-End/middleware/auth.js
@@ -90,3 +90,45 @@ module.exports.verifyAdmin=  async (req, res, next) => {
     }
    }
 
+
+//    module.exports.varifyU=function(req,res,next){
+//     try{
+//         const token=req.headers.authorization.split(" ")[1];
+//         console.log(token)
+//         const decodedData=jwt.verify(token,'secretkey');
+//         User.findById({_id:decodedData.userId}).then(function(alldata){
+//             req.user=alldata;
+//             next()
+//         }).catch(function(err){
+//             return res.status(201).json({success:true,msg:"Unauthorized access!!"})
+//         })
+//     }
+//     catch(err){
+//         return res.status(201).json({success:true,msg:"Unauthorized access!!"})
+//     }
+// }
+
+// auth token verify function
+module.exports.varifyU = function(req,res,next){
+    try{
+        const token = req.headers.authorization.split(" ")[1]; //token only
+        console.log(token)
+        const decodeData = jwt.verify(token, 'secretkey')
+        User.findOne({_id : decodeData.userId})
+        .then(function(result){
+            //success
+            req.user = result
+            next()
+            
+           
+        }
+    )
+        .catch(function(err){
+            res.status(401).json({message : err})
+        })
+    }
+    catch(err){
+        res.status(401).json({message : "Unauthorized access!!"})
+    }
+}
+
